ec8bb7b6f27b7fa019cbea1e973852f5bc7dcd82
Zoli068 <lackozoli1@gmail.com>
2025-09-27 23:57:43 +0200

    GUI at the slave side
    Added GUI at the slave side to be able to see the current values at the slave side

--- Common/InMemoryDatabases/PointsDataBase/PointsDataBase.cs
+++ Common/InMemoryDatabases/PointsDataBase/PointsDataBase.cs
@@ -1,5 +1,7 @@
 ﻿using Common.IPointsDataBase;
+using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Common.PointsDataBase
 {
@@ -8,8 +10,10 @@ namespace Common.PointsDataBase
     /// </summary>
     public class PointsDataBase : IPointsDataBase.IPointsDataBase
     {
-        private Dictionary<ushort, (PointsType, short)> RegistersDictionary = new Dictionary<ushort, (PointsType, short)>();
-        private Dictionary<ushort, (PointsType, byte)> DiscreteDictionary = new Dictionary<ushort, (PointsType, byte)>();
+        public event Action DataChanged;
+
+        public Dictionary<ushort, (PointsType, short)> RegistersDictionary = new Dictionary<ushort, (PointsType, short)>();
+        public Dictionary<ushort, (PointsType, byte)> DiscreteDictionary = new Dictionary<ushort, (PointsType, byte)>();
 
         public PointsDataBase()
         {
@@ -83,6 +87,7 @@ namespace Common.PointsDataBase
             {
                 if (point.Item1 == pointType)
                 {
+                    OnDataChanged();
                     point.Item2 = value;
                     DiscreteDictionary[address] = point;
                 }
@@ -108,6 +113,7 @@ namespace Common.PointsDataBase
                 {
                     if (point.Item1 == pointType)
                     {
+                        OnDataChanged();
                         point.Item2 = value;
                         RegistersDictionary[address] = point;
                     }
@@ -129,93 +135,48 @@ namespace Common.PointsDataBase
 
         private void InitializePoints()
         {
-            // Discrete Outputs (Coils) - Addresses 00001 to 10000
-            DiscreteDictionary.Add(1, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(2, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(3, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(4, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(5, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(6, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(7, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(8, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(9, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(10, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(11, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(12, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(13, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(14, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(15, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(16, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(17, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(18, (PointsType.COILS, 1));
-            DiscreteDictionary.Add(19, (PointsType.COILS, 0));
-            DiscreteDictionary.Add(20, (PointsType.COILS, 1));
+            InitializePointsFromFile("pointsConfiguration.txt");
+        }
 
-            // Discrete Inputs - Addresses 10001 to 20000
-            DiscreteDictionary.Add(10001, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10002, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10003, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10004, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10005, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10006, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10007, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10008, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10009, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10010, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10011, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10012, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10013, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10014, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10015, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10016, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10017, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10018, (PointsType.DISCRETE_INPUTS, 0));
-            DiscreteDictionary.Add(10019, (PointsType.DISCRETE_INPUTS, 1));
-            DiscreteDictionary.Add(10020, (PointsType.DISCRETE_INPUTS, 0));
+        private void OnDataChanged()
+        {
+            DataChanged?.Invoke();
+        }
 
-            // Input Registers - Addresses 30001 to 40000
-            RegistersDictionary.Add(30001, (PointsType.INPUT_REGISTERS, 100));
-            RegistersDictionary.Add(30002, (PointsType.INPUT_REGISTERS, 200));
-            RegistersDictionary.Add(30003, (PointsType.INPUT_REGISTERS, 300));
-            RegistersDictionary.Add(30004, (PointsType.INPUT_REGISTERS, 400));
-            RegistersDictionary.Add(30005, (PointsType.INPUT_REGISTERS, 500));
-            RegistersDictionary.Add(30006, (PointsType.INPUT_REGISTERS, 600));
-            RegistersDictionary.Add(30007, (PointsType.INPUT_REGISTERS, 700));
-            RegistersDictionary.Add(30008, (PointsType.INPUT_REGISTERS, 800));
-            RegistersDictionary.Add(30009, (PointsType.INPUT_REGISTERS, 900));
-            RegistersDictionary.Add(30010, (PointsType.INPUT_REGISTERS, 1000));
-            RegistersDictionary.Add(30011, (PointsType.INPUT_REGISTERS, 1100));
-            RegistersDictionary.Add(30012, (PointsType.INPUT_REGISTERS, 1200));
-            RegistersDictionary.Add(30013, (PointsType.INPUT_REGISTERS, 1300));
-            RegistersDictionary.Add(30014, (PointsType.INPUT_REGISTERS, 1400));
-            RegistersDictionary.Add(30015, (PointsType.INPUT_REGISTERS, 1500));
-            RegistersDictionary.Add(30016, (PointsType.INPUT_REGISTERS, 1600));
-            RegistersDictionary.Add(30017, (PointsType.INPUT_REGISTERS, 1700));
-            RegistersDictionary.Add(30018, (PointsType.INPUT_REGISTERS, 1800));
-            RegistersDictionary.Add(30019, (PointsType.INPUT_REGISTERS, 1900));
-            RegistersDictionary.Add(30020, (PointsType.INPUT_REGISTERS, 2000));
+        private void InitializePointsFromFile(string filePath)
+        {
+            foreach (var line in File.ReadLines(filePath))
+            {
+                if (string.IsNullOrWhiteSpace(line) || line.StartsWith("#"))
+                    continue;
 
-            // Holding Registers - Addresses 40001 to 50000
-            RegistersDictionary.Add(40001, (PointsType.HOLDING_REGISTERS, 1000));
-            RegistersDictionary.Add(40002, (PointsType.HOLDING_REGISTERS, 1100));
-            RegistersDictionary.Add(40003, (PointsType.HOLDING_REGISTERS, 1200));
-            RegistersDictionary.Add(40004, (PointsType.HOLDING_REGISTERS, 1300));
-            RegistersDictionary.Add(40005, (PointsType.HOLDING_REGISTERS, 1400));
-            RegistersDictionary.Add(40006, (PointsType.HOLDING_REGISTERS, 1500));
-            RegistersDictionary.Add(40007, (PointsType.HOLDING_REGISTERS, 1600));
-            RegistersDictionary.Add(40008, (PointsType.HOLDING_REGISTERS, 1700));
-            RegistersDictionary.Add(40009, (PointsType.HOLDING_REGISTERS, 1800));
-            RegistersDictionary.Add(40010, (PointsType.HOLDING_REGISTERS, 1900));
-            RegistersDictionary.Add(40011, (PointsType.HOLDING_REGISTERS, 2000));
-            RegistersDictionary.Add(40012, (PointsType.HOLDING_REGISTERS, 2100));
-            RegistersDictionary.Add(40013, (PointsType.HOLDING_REGISTERS, 2200));
-            RegistersDictionary.Add(40014, (PointsType.HOLDING_REGISTERS, 2300));
-            RegistersDictionary.Add(40015, (PointsType.HOLDING_REGISTERS, 2400));
-            RegistersDictionary.Add(40016, (PointsType.HOLDING_REGISTERS, 2500));
-            RegistersDictionary.Add(40017, (PointsType.HOLDING_REGISTERS, 2600));
-            RegistersDictionary.Add(40018, (PointsType.HOLDING_REGISTERS, 2700));
-            RegistersDictionary.Add(40019, (PointsType.HOLDING_REGISTERS, 2800));
-            RegistersDictionary.Add(40020, (PointsType.HOLDING_REGISTERS, 2900));
+                var parts = line.Split(new[] { ' ', '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 3)
+                    throw new FormatException($"Invalid line format: {line}");
+
+                if (!ushort.TryParse(parts[0], out ushort address))
+                    throw new FormatException($"Invalid address: {parts[0]}");
+
+                if (!Enum.TryParse(parts[1], out PointsType type))
+                    throw new FormatException($"Invalid PointsType: {parts[1]}");
+
+                if (!int.TryParse(parts[2], out int value))
+                    throw new FormatException($"Invalid value: {parts[2]}");
+
+                if (type == PointsType.COILS || type == PointsType.DISCRETE_INPUTS)
+                {
+                    DiscreteDictionary[address] = (type, (byte)value);
+                }
+                else if (type == PointsType.INPUT_REGISTERS || type == PointsType.HOLDING_REGISTERS)
+                {
+                    RegistersDictionary[address] = (type, (short)value);
+                }
+                else
+                {
+                    throw new ArgumentException($"Unsupported point type: {type}");
+                }
+            }
         }
     }
 }
---
+++ SlaveGUI/MainWindow.xaml.cs
@@ -0,0 +1,76 @@
+﻿using Common.Communication;
+using Common.CommunicationExceptions;
+using Common.FIFOQueue;
+using Common.FileRecord;
+using Common.Message;
+using Common.PointsDataBase;
+using Slave.Communication;
+using Slave.Message;
+using System;
+using System.Linq;
+using System.Windows;
+
+namespace SlaveGUI
+{
+    public partial class MainWindow : Window
+    {
+        ICommunication communication;
+        IMessageProcesser<FunctionCode> messageProcesser;
+        PointsDataBase db = new PointsDataBase();
+        IFileRecord fileRecord = new FileRecord();
+        IFIFOQueue fIFOQueue = new FIFOQueue();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Start();
+            db.DataChanged +=RefreshTables;
+            RefreshTables(); 
+        }
+
+        private void RefreshTables() 
+        {
+            Application.Current.Dispatcher.BeginInvoke(new Action(() =>
+            {
+                RegistersDataGrid.ItemsSource = null;
+                RegistersDataGrid.ItemsSource = db.RegistersDictionary
+                    .Select(kvp => new
+                    {
+                        Address = kvp.Key,
+                        Type = kvp.Value.Item1,
+                        Value = kvp.Value.Item2
+                    })
+                    .ToList();
+
+                DiscretesDataGrid.ItemsSource = null;
+                DiscretesDataGrid.ItemsSource = db.DiscreteDictionary
+                    .Select(kvp => new
+                    {
+                        Address = kvp.Key,
+                        Type = kvp.Value.Item1,
+                        Value = kvp.Value.Item2
+                    })
+                    .ToList();
+            }));
+        }
+
+        private void Start()
+        {
+            try
+            {
+                communication = new Communication();
+                messageProcesser = new ModbusMessageProcesser(db, fileRecord, fIFOQueue, communication);
+            }
+            catch (Exception ex) when (ex is ListeningNotSuccessedException)
+            {
+                Console.WriteLine("The server was not able to start");
+                Console.WriteLine(ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An unknown error happened");
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
